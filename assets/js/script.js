886490ef8c2cc4bf17de03f9729ea20b80804ce6
awaisqamar193-droid <awaisqamar193@gmail.com>
2026-05-06 20:55:54 +0500

    aos library

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,31 @@ window.addEventListener("scroll", function () {
     header.classList.remove("sticky");
   }
 });
+
+
+let currentPage = window.location.pathname.split("/").pop();
+
+// agar home page ho (empty URL)
+if (currentPage === "") {
+  currentPage = "index.html";
+}
+
+let navLinks = document.querySelectorAll(".navbar .nav-link");
+
+navLinks.forEach(function(link) {
+  let linkHref = link.getAttribute("href");
+
+  if (linkHref === currentPage) {
+    link.classList.add("active");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  let btn = document.getElementById("fixed-tag");
+
+  if (window.scrollY > 50) {
+    btn.classList.add("show");
+  } else {
+    btn.classList.remove("show");
+  }
+});
